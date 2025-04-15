f4611137164428c53fa90052ecc3bba5c0c78c2f
Kristopher Krasnosky <kris@seaward.science>
2025-04-15 16:04:11 -0400

    nav launch working

--- echoflow/launch/microstrain.launch.py
+++ echoflow/launch/microstrain.launch.py
@@ -1,7 +1,6 @@
 # Standalone example launch file for GX3, GX4, GX/CX5, RQ1 and GQ7 series devices
 # Note: Feature support is device-dependent and some of the following settings may have no affect on your device.
 # Please consult your device's documentation for supported features
-
 import os
 import yaml
 from launch import LaunchDescription
@@ -11,7 +10,6 @@ from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch.events import matches_action
 from launch_ros.actions import LifecycleNode
 from launch_ros.events.lifecycle import ChangeState
-
 from lifecycle_msgs.msg import Transition
 from ament_index_python.packages import get_package_share_directory
 
@@ -34,10 +32,9 @@ _TREVOR_PARAMS_FILE = os.path.join(
 )
 
 def generate_launch_description():
-
   # Declare arguments with default values
   launch_description = []
-  launch_description.append(DeclareLaunchArgument('namespace',   default_value='/t1/nav/sensors',                description='Namespace to use when launching the nodes in this launch file'))
+  launch_description.append(DeclareLaunchArgument('vessel_ns',   default_value='aura',                description='Vessel namespace'))
   launch_description.append(DeclareLaunchArgument('node_name',   default_value='microstrain',      description='Name to give the Microstrain Inertial Driver node'))
   launch_description.append(DeclareLaunchArgument('configure',   default_value='true',            description='Whether or not to configure the node on startup'))
   launch_description.append(DeclareLaunchArgument('activate',    default_value='true',            description='Whether or not to activate the node on startup'))
@@ -46,14 +43,14 @@ def generate_launch_description():
   # Pass an environment variable to the node to determine if it is in debug or not
   launch_description.append(SetEnvironmentVariable('MICROSTRAIN_INERTIAL_DEBUG', value=LaunchConfiguration('debug')))
 
-  # ****************************************************************** 
-  # Microstrain sensor node 
-  # ****************************************************************** 
+  # ******************************************************************
+  # Microstrain sensor node
+  # ******************************************************************
   microstrain_node = LifecycleNode(
     package    = _PACKAGE_NAME,
     executable = "microstrain_inertial_driver_node",
     name       = LaunchConfiguration('node_name'),
-    namespace  = LaunchConfiguration('namespace'),
+    namespace  = PythonExpression(["'/", LaunchConfiguration('vessel_ns'), "/nav/sensors/mru'"]),
     respawn=True,
     respawn_delay=4,
     parameters = [
@@ -65,7 +62,12 @@ def generate_launch_description():
 
       # Supported overrides
       {
-        "debug" : LaunchConfiguration('debug')
+        "debug" : LaunchConfiguration('debug'),
+        # Frame IDs with vessel_ns as prefix
+        "mount_frame_id": PythonExpression(["'", LaunchConfiguration('vessel_ns'), "/base_link'"]),
+        "gnss1_frame_id": PythonExpression(["'", LaunchConfiguration('vessel_ns'), "/gnss_1_antenna_link'"]),
+        "gnss2_frame_id": PythonExpression(["'", LaunchConfiguration('vessel_ns'), "/gnss_2_antenna_link'"]),
+        "odometer_frame_id": PythonExpression(["'", LaunchConfiguration('vessel_ns'), "/odometer_link'"]),
       },
     ]
   )
@@ -90,7 +92,3 @@ def generate_launch_description():
   launch_description.append(config_event)
   launch_description.append(activate_event)
   return LaunchDescription(launch_description)
-  
-
- 
- 
