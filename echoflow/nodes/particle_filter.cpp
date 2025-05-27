3bc2039dcb7deeaa1871c3f7b1740306102ceb78
antonellla <antonella.c.wilby@gmail.com>
2025-05-26 17:29:36 -0700

    test multi-threaded execution

--- echoflow/nodes/particle_filter.cpp
+++ echoflow/nodes/particle_filter.cpp
@@ -9,8 +9,8 @@ int main(int argc, char **argv) {
 
   filter_node->map_ptr_ = grid_node->getMapPtr();
 
-  //rclcpp::executors::MultiThreadedExecutor executor;
-  rclcpp::executors::SingleThreadedExecutor executor;
+  rclcpp::executors::MultiThreadedExecutor executor;
+  //rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(filter_node);
   executor.add_node(grid_node);
   executor.spin();
